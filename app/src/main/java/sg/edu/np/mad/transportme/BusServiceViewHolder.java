1e1529645b6824c62a94f5f2e1c279ab6378ef1c
dalberrt <dalbertchea@gmail.com>
2022-06-15 00:02:02 +0800

    Improved Bus stop timing UI, implemented text color change according to occupancy and bus type display.

--- app/src/main/java/sg/edu/np/mad/transportme/BusServiceViewHolder.java
+++ app/src/main/java/sg/edu/np/mad/transportme/BusServiceViewHolder.java
@@ -10,6 +10,9 @@ public class BusServiceViewHolder extends RecyclerView.ViewHolder{
     TextView NextBus1;
     TextView NextBus2;
     TextView NextBus3;
+    TextView NextBus1Type;
+    TextView NextBus2Type;
+    TextView NextBus3Type;
 
     public BusServiceViewHolder(View item){
         super(item);
@@ -17,6 +20,9 @@ public class BusServiceViewHolder extends RecyclerView.ViewHolder{
         NextBus1 = item.findViewById(R.id.NextBus1);
         NextBus2 = item.findViewById(R.id.NextBus2);
         NextBus3 = item.findViewById(R.id.NextBus3);
+        NextBus1Type = item.findViewById(R.id.NextBus1Type);
+        NextBus2Type = item.findViewById(R.id.NextBus2Type);
+        NextBus3Type = item.findViewById(R.id.NextBus3Type);
 
     }
 }
