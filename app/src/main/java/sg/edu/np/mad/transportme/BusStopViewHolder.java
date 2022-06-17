74240585b84dfd44a34a4aa3e69a29fc5b7ff302
dalberrt <dalbertchea@gmail.com>
2022-06-17 16:28:48 +0800

    Implemented Favourites function + Updates to Firebase, requires testing after firebase is settled.
    Recyclingview issue yet to be solved.

--- app/src/main/java/sg/edu/np/mad/transportme/BusStopViewHolder.java
+++ app/src/main/java/sg/edu/np/mad/transportme/BusStopViewHolder.java
@@ -1,6 +1,7 @@
 package sg.edu.np.mad.transportme;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,12 +10,14 @@ public class BusStopViewHolder extends RecyclerView.ViewHolder{
     TextView Description;
     TextView BusStopCode;
     TextView RoadName;
+    ImageView Favourite;
     RecyclerView RecyclerView2;
     public BusStopViewHolder(View item){
         super(item);
         Description = item.findViewById(R.id.Description);
         BusStopCode = item.findViewById(R.id.BusStopCode);
         RoadName = item.findViewById(R.id.RoadName);
+        Favourite = item.findViewById(R.id.Favourite);
         RecyclerView2 = item.findViewById(R.id.recyclerView2);
     }
 
