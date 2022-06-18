6c55eff714dce905576289edd14a9d5b2b2a1168
kyler <s10222789@connect.np.edu.sg>
2022-06-18 23:07:50 +0800

    sign out button working for profiles

--- app/src/main/java/sg/edu/np/mad/transportme/ProfileFragment.java
+++ app/src/main/java/sg/edu/np/mad/transportme/ProfileFragment.java
@@ -1,5 +1,6 @@
 package sg.edu.np.mad.transportme;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +61,16 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        Intent intent = new Intent(getActivity(), LoginPage.class);
+        Button signoutButton = rootView.findViewById(R.id.signoutbutton);
+        signoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+        return rootView;
+
     }
 }
