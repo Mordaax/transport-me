570886c93c83a1961d617c97f39a8fbc758ed201
Mordax <cyrustan77@gmail.com>
2022-06-19 18:36:41 +0800

    Added sorting for nearest bus stop, Progress dialogbar for first page

--- app/src/main/java/sg/edu/np/mad/transportme/BusStop.java
+++ app/src/main/java/sg/edu/np/mad/transportme/BusStop.java
@@ -2,7 +2,7 @@ package sg.edu.np.mad.transportme;
 
 import java.util.ArrayList;
 
-public class BusStop {
+public class BusStop implements Comparable<BusStop> {
     String BusStopCode;
     String RoadName;
     String Description;
@@ -10,6 +10,7 @@ public class BusStop {
     Double Longitude;
     Boolean Favourited;
     ArrayList<BusService> busServices;
+    Double distanceToLocation;
 
     public BusStop(String BusStopCode, String RoadName, String Description, Double Latitude, Double Longitude){
         this.BusStopCode = BusStopCode;
@@ -17,5 +18,16 @@ public class BusStop {
         this.Description = Description;
         this.Latitude = Latitude;
         this.Longitude = Longitude;
+    }
+    public int compareTo(BusStop bs){
+        if(distanceToLocation == bs.distanceToLocation){
+            return 0;
+        }
+        else if (distanceToLocation > bs.distanceToLocation){
+            return 1;
+        }
+        else{
+            return -1;
+        }
     }
 }
