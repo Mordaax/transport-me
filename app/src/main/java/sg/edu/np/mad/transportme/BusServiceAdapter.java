666a43606aa59c684f9b09773f37d2f9503ce430
dalberrt <dalbertchea@gmail.com>
2022-06-26 18:41:47 +0800

    Bus Service Adapter Comments

--- app/src/main/java/sg/edu/np/mad/transportme/BusServiceAdapter.java
+++ app/src/main/java/sg/edu/np/mad/transportme/BusServiceAdapter.java
@@ -14,17 +14,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BusServiceAdapter
-        extends RecyclerView.Adapter<BusServiceViewHolder>        //just like list, need declare <data type>
+        extends RecyclerView.Adapter<BusServiceViewHolder>
 {
     ArrayList<BusService> data;
     public BusServiceAdapter(ArrayList<BusService> data)
     {
-        this.data = data;                                       //this = this class / object
-    }
-    /* Remove This????? */
-    @Override
-    public int getItemViewType(int position){
-        return (position%5==0)?0:1;
+        this.data = data;
     }
 
     @NonNull
@@ -45,76 +40,80 @@ public class BusServiceAdapter
             }
         });
 
-        holder.ServiceNumber.setText(content.getServiceNumber());
+        holder.ServiceNumber.setText(content.getServiceNumber());       //Set the Text of Bus Service Number
+        //Populating the list with each attribute of the next bus (Bus, Type, Feature)
         ArrayList<TextView> nextBusList= new ArrayList<TextView>(Arrays.asList(holder.NextBus1, holder.NextBus2, holder.NextBus3));
         ArrayList<TextView> nextBusTypeList = new ArrayList<TextView>(Arrays.asList(holder.NextBus1Type, holder.NextBus2Type, holder.NextBus3Type));
         ArrayList<ImageView> nextBusFeatureList = new ArrayList<>(Arrays.asList(holder.NextBus1Feature, holder.NextBus2Feature, holder.NextBus3Feature));
-        for (int i = 0; i < nextBusList.size(); i++)
+        for (int i = 0; i < nextBusList.size(); i++)        //Loops through the 3 next busses
         {
+            //Declaring the next bus
             NextBus nb = content.getNextBuses().get(i);
+            //Assigning variables to their corresponding holder views
             TextView busTV = nextBusList.get(i);
             TextView busTypeTV = nextBusTypeList.get(i);
             ImageView busFeatureIV = nextBusFeatureList.get(i);
-            if (nb.getEstimatedArrival() == "Null")
+            if (nb.getEstimatedArrival() == "Null")                 //Checks if the bus exists (Null == non-existent)
             {
-                busTV.setTextColor(Color.parseColor("#000000"));
+                busTV.setTextColor(Color.parseColor("#000000"));    //Sets the text to 一 in black to indicate no bus
                 busTV.setText("一");
                 busTypeTV.setText("");
                 busFeatureIV.setVisibility(View.GONE);
                 continue;
             }
-            else if (Integer.parseInt(nb.getEstimatedArrival()) == 0)
+            else if (Integer.parseInt(nb.getEstimatedArrival()) == 0)       //When 0 minutes is left to arrival, set to Arr
             {
                 busTV.setText("Arr");
             }
-            else if (Integer.parseInt(nb.getEstimatedArrival()) < 0)
+            else if (Integer.parseInt(nb.getEstimatedArrival()) < 0)        //If estimated arrival is negative, set to Left
             {
                 busTV.setText("Left");
             }
             else
             {
-                nextBusList.get(i).setText(nb.getEstimatedArrival());
+                nextBusList.get(i).setText(nb.getEstimatedArrival());       //Else set it to estimated arrival time in mins
             }
 
-            if (nb.getFeature().equals("none"))
+            if (nb.getFeature().equals("none"))                             //Checks if bus is WAB (Wheel chair accessible)
             {
-                busFeatureIV.setVisibility(View.GONE);
+                busFeatureIV.setVisibility(View.GONE);                      //Does not display WAB icon if none
             }
 
-            if (nb.getLoad().equals("SEA"))
+            if (nb.getLoad().equals("SEA"))     //SEA = Seats Available
             {
-                busTV.setTextColor(Color.parseColor("#90a959"));
+                busTV.setTextColor(Color.parseColor("#90a959"));           //Sets text color to green (Green = Not Full)
             }
-            else if (nb.getLoad().equals("SDA"))
+            else if (nb.getLoad().equals("SDA"))    //SDA = Standing Available
             {
-                busTV.setTextColor(Color.parseColor("#e9b872"));
-                busFeatureIV.setImageResource(R.drawable.wheelchair_yellow);
+                busTV.setTextColor(Color.parseColor("#e9b872"));           //Sets text color to yellow (Yellow = medium fullness)
+                busFeatureIV.setImageResource(R.drawable.wheelchair_yellow);        //Sets WAB icon to yellow
             }
-            else
+            else        //Else = LSD = Limited Standing
             {
-                busTV.setTextColor(Color.parseColor("#a63d40"));
-                busFeatureIV.setImageResource(R.drawable.wheelchair_red);
+                busTV.setTextColor(Color.parseColor("#a63d40"));          //Sets text color to red (Red = Extremely full)
+                busFeatureIV.setImageResource(R.drawable.wheelchair_red);          //sets WAB icon to red
             }
 
-            if (nb.getType().equals("SD"))
+            if (nb.getType().equals("SD"))      //SD = Single Deck
             {
-                busTypeTV.setText("Single");
+                busTypeTV.setText("Single");    //Displays bus type as Single
             }
-            else if (nb.getType().equals("DD"))
+            else if (nb.getType().equals("DD")) //DD = Double Deck
             {
-                busTypeTV.setText("Double");
+                busTypeTV.setText("Double");    //Displays bus type as Double
             }
-            else
+            else        //Else = BD = Bendy
             {
-                busTypeTV.setText("Bendy");
+                busTypeTV.setText("Bendy");     //Displays bus type as Bendy
             }
 
 
         }
 
-        //holder.NextBus1.setText(content.NextBuses.get(0).EstimatedArrival);
-        //holder.NextBus2.setText(content.NextBuses.get(1).EstimatedArrival);
-        //holder.NextBus3.setText(content.NextBuses.get(2).EstimatedArrival);
+        /*
+        Load: SEA (Seats available), SDA (Standing available), LSD (Limited Standing)
+        Feature: WAB (wheel Chair accessible), none (Not wheel chat accessible)
+        Type: SD (Single Deck), DD (Double Deck), BD (Bendy) */
     }
 
     @Override
