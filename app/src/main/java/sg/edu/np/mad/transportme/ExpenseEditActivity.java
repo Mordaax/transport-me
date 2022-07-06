c36cb33375c9642082a3430fe1c9e9a5fbd45a3f
kyler <s10222789@connect.np.edu.sg>
2022-07-06 23:57:30 +0800

    Created expense logging UI

--- app/src/main/java/sg/edu/np/mad/transportme/ExpenseEditActivity.java
+++ app/src/main/java/sg/edu/np/mad/transportme/ExpenseEditActivity.java
@@ -3,12 +3,28 @@ package sg.edu.np.mad.transportme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class ExpenseEditActivity extends AppCompatActivity {
 
+    String[] items = {"Bus", "Train", "Taxi", "Others"};
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> adapterItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense_edit);
+        autoCompleteTextView = findViewById(R.id.auto_complete);
+        adapterItems = new ArrayAdapter<String>(this, R.layout.list_item_layout, items);
+        autoCompleteTextView.setAdapter(adapterItems);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selected = adapterView.getItemAtPosition(i).toString();
+            }
+        });
     }
 }
