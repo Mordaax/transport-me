6c278ccc042af8adc4bc1dcb66cb442d14458a4a
loghogjog <s10223003@connect.np.edu.sg>
2022-07-08 22:46:06 +0800

    Initial commit for carpark feature

---
+++ app/src/main/java/sg/edu/np/mad/transportme/CarparkViewHolder.java
@@ -0,0 +1,12 @@
+package sg.edu.np.mad.transportme;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CarparkViewHolder extends RecyclerView.ViewHolder {
+    public CarparkViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
