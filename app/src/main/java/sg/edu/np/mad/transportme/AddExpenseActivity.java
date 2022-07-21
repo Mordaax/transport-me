ac50dd7e09341ff61f58a9269606527921892d5a
kyler <s10222789@connect.np.edu.sg>
2022-07-22 01:59:46 +0800

    Expenses storing and pulling from database configurations created

--- app/src/main/java/sg/edu/np/mad/transportme/AddExpenseActivity.java
+++ app/src/main/java/sg/edu/np/mad/transportme/AddExpenseActivity.java
@@ -1,5 +1,7 @@
 package sg.edu.np.mad.transportme;
 
+import static sg.edu.np.mad.transportme.user.LoginPage.globalName;
+
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,12 +15,14 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class AddExpenseActivity extends AppCompatActivity {
-
+    FirebaseDatabase db = FirebaseDatabase.getInstance("https://transportme-c607f-default-rtdb.asia-southeast1.firebasedatabase.app/");
     String[] items = {"Bus", "Train", "Taxi", "Others"};
     AutoCompleteTextView autoCompleteTextView;
     ArrayAdapter<String> adapterItems;
@@ -26,7 +30,6 @@ public class AddExpenseActivity extends AppCompatActivity {
     TextView expenseDate, expenseTime;
     TextInputEditText expenseCost;
     DateTimeFormatter dateFormatter, timeFormatter;
-    LocalTime time;
     Button addExpense;
 
     @Override
@@ -34,13 +37,11 @@ public class AddExpenseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense_edit);
         expenseDate = findViewById(R.id.expenseDate);
-        expenseTime = findViewById(R.id.expenseTime);
         expenseCost = findViewById(R.id.expenseCost);
         addExpense = findViewById(R.id.addExpense);
         dateFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
         timeFormatter = DateTimeFormatter.ofPattern("hh:mm a");
         autoCompleteTextView = findViewById(R.id.auto_complete);
-        time = LocalTime.now();
         adapterItems = new ArrayAdapter<String>(this, R.layout.list_item_layout, items);
 
         autoCompleteTextView.setAdapter(adapterItems);
@@ -51,13 +52,30 @@ public class AddExpenseActivity extends AppCompatActivity {
             }
         });
         expenseDate.setText("DATE: " + WeekUtils.dateSelected.format(dateFormatter));
-        expenseTime.setText("TIME: " + time.format(timeFormatter));
         addExpense.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String cost = expenseCost.getText().toString();
-                Expense expense = new Expense(WeekUtils.dateSelected, time, cost, selected);
+                if(cost.isEmpty()){
+                    expenseCost.setError("Please enter a cost");
+                    expenseCost.requestFocus();
+                    return;
+                }
+                if(selected == null){
+                    autoCompleteTextView.setError("Please enter a mode of transport");
+                    autoCompleteTextView.requestFocus();
+                    return;
+                }
+                Expense expense = new Expense(WeekUtils.dateSelected, cost, selected);
                 Expense.expenseArrayList.add(expense);
+                DatabaseReference reference = db.getReference()
+                        .child("User")
+                        .child(globalName)
+                        .child("Expenses");
+                String id = reference.push().getKey();
+                reference.child(id).child("Date").setValue(WeekUtils.dateSelected.toString());
+                reference.child(id).child("Cost").setValue(cost);
+                reference.child(id).child("Selected").setValue(selected);
                 finish();
             }
         });
--- app/src/main/java/sg/edu/np/mad/transportme/WeekActivity.java
+++ app/src/main/java/sg/edu/np/mad/transportme/WeekActivity.java
@@ -1,44 +1,61 @@
 package sg.edu.np.mad.transportme;
 
+import static sg.edu.np.mad.transportme.user.LoginPage.globalName;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class WeekActivity extends AppCompatActivity implements WeekAdapter.ItemListener {
     private RecyclerView calendarRV;
     private ListView weekListView;
-    private TextView weekText;
+    private TextView weekText, ttl;
     private Button weekBefore, weekAfter, log;
+    public static Boolean arraySet = false;
 
+    FirebaseDatabase db = FirebaseDatabase.getInstance("https://transportme-c607f-default-rtdb.asia-southeast1.firebasedatabase.app/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_week);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
         calendarRV = findViewById(R.id.calendarRV);
         weekListView = findViewById(R.id.weekListView);
         weekText = findViewById(R.id.yearMonth);
         weekBefore = findViewById(R.id.weekBefore);
         weekAfter = findViewById(R.id.weekAfter);
         log = findViewById(R.id.log);
+        ttl = findViewById(R.id.total);
         Intent intent = new Intent(this, AddExpenseActivity.class);
         WeekUtils.dateSelected = LocalDate.now();
+        setExpenseArray();
         setWeek();
+
+
 
         weekBefore.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -113,5 +130,49 @@ public class WeekActivity extends AppCompatActivity implements WeekAdapter.ItemL
         ArrayList<Expense> daysExpense = Expense.expensePerDate(WeekUtils.dateSelected);
         ExpenseAdapter expenseAdapter = new ExpenseAdapter(getApplicationContext(), daysExpense);
         weekListView.setAdapter(expenseAdapter);
+        int totalcost = 0;
+        for (Expense expense : daysExpense){
+            int cost = Integer.parseInt(expense.getCost());
+            totalcost += cost;
+        }
+        ttl.setText("Total: $"+totalcost);
+
+    }
+
+    private void setExpenseArray() {
+        DatabaseReference reference = db.getReference() //Database Reference expense
+                .child("User")
+                .child(globalName)
+                .child("Expenses");
+        Log.w("check","check");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (!arraySet){
+                    for (DataSnapshot expenses: snapshot.getChildren()) {
+                        Log.w("date", expenses.child("Date").getValue().toString());
+                        Log.w("cost", expenses.child("Cost").getValue().toString());
+                        Log.w("selected", expenses.child("Selected").getValue().toString());
+                        LocalDate date = LocalDate.parse(expenses.child("Date").getValue().toString());
+                        Expense expense = new Expense(date, expenses.child("Cost").getValue().toString(), expenses.child("Selected").getValue().toString());
+                        Expense.expenseArrayList.add(expense);
+                    }
+                }
+                SharedPreferences prefs =  getSharedPreferences("ExpenseData", MODE_PRIVATE);
+                arraySet = prefs.getString("arraySet", "").equals("");
+                Log.w("s", arraySet.toString());
+                setEventAdapter();
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+
+        });
+
+
     }
 }
