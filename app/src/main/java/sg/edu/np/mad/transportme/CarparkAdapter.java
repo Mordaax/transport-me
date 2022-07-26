a5be34a024d43859e4fd407b79242d46c1054f15
loghogjog <s10223003@connect.np.edu.sg>
2022-07-26 13:14:27 +0800

    commit for carpark recyclerview, adapter, class

--- app/src/main/java/sg/edu/np/mad/transportme/CarparkAdapter.java
+++ app/src/main/java/sg/edu/np/mad/transportme/CarparkAdapter.java
@@ -30,12 +30,13 @@ import sg.edu.np.mad.transportme.api.MySingleton;
 public class CarparkAdapter
         extends RecyclerView.Adapter<CarparkViewHolder> {
 
-    ArrayList<Carpark> data = getCarparkAvailability();
+    ArrayList<Carpark> data;
     Context c;
-    JsonObjectRequest jsonObjectRequestCarPark;
+    //JsonObjectRequest jsonObjectRequestCarPark;
 
-    public CarparkAdapter(Context c){
+    public CarparkAdapter(Context c, ArrayList<Carpark> data){
         this.c = c;
+        this.data = data;
     }
 
     @NonNull
@@ -68,68 +69,11 @@ public class CarparkAdapter
             holder.TruckLotsAvailable.setText(carpark_content.AvailableLots);
         }
 
-
-        /**
-        BusServiceAdapter adapterMember = new BusServiceAdapter(content.getBusServices(), c);  //Create the RecyclerView for BusServices
-        LinearLayoutManager layout = new LinearLayoutManager(c);                            //LayoutManager tells RecyclerView how to draw the list
-
-        holder.RecyclerView2.setLayoutManager(layout);          //Pass in layout and adapter
-        holder.RecyclerView2.setAdapter(adapterMember);
-         **/
     }
 
     @Override
     public int getItemCount() {
         return data.size();
-    }
-
-    public ArrayList<Carpark> getCarparkAvailability(){
-        String url = "http://datamall2.mytransport.sg/ltaodataservice/CarParkAvailabilityv2";
-        //Get carparks using api
-        jsonObjectRequestCarPark = new JsonObjectRequest( Request.Method.GET, url,null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONArray jsonArray = response.getJSONArray("value");
-                            for (int i = 0; i < jsonArray.length(); i++){
-                                //converting JSONArray to JSONObject
-                                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                int carparkID =  Integer.parseInt(jsonObject.get("CarParkID").toString());
-                                String area = jsonObject.get("Area").toString();
-                                String dev = jsonObject.get("Development").toString();
-                                String[] coordinates = jsonObject.get("Location").toString().split(" ");
-                                Location location = new Location(dev);
-                                location.setLatitude(Double.parseDouble(coordinates[0]));
-                                location.setLongitude(Double.parseDouble(coordinates[1]));
-                                int available = Integer.parseInt(jsonObject.get("AvailableLots").toString());
-                                String lotType = jsonObject.get("LotType").toString();
-                                String agency = jsonObject.get("Agency").toString();
-                                //Adding object to array
-                                data.add(new Carpark(carparkID, area, dev, location, available, lotType, agency));
-
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        Log.d("TAG", response.toString());
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("TAG", error.getMessage(), error);
-            }
-        }) { //no semicolon or coma
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("AccountKey", "MlHNjn00RVKWi4Z1R5zR+A==");
-                params.put("accept", "application/json");
-                return params;
-            }
-        };
-        MySingleton.getInstance(c).addToRequestQueue(jsonObjectRequestCarPark);
-        return data;
     }
 
 
