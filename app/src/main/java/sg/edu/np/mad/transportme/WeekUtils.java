31ea7698e546b86a87c66e32dba0e671bf9c12aa
kyler <s10222789@connect.np.edu.sg>
2022-07-31 01:50:29 +0800

    More comments

--- app/src/main/java/sg/edu/np/mad/transportme/WeekUtils.java
+++ app/src/main/java/sg/edu/np/mad/transportme/WeekUtils.java
@@ -2,7 +2,7 @@ package sg.edu.np.mad.transportme;
 
 import java.time.LocalDate;
 
-public class WeekUtils {
+public class WeekUtils { //as dateSelected is used across many activities, instead of constantly passing the value over, define it in WeekUtils
     public static LocalDate dateSelected;
 }
 
